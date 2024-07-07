2ca924efd2ebea80f69a63dc0fd52af81219a1be
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-07 21:55:20 +0200

    feat: add page link and info section components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
 import ProductGallery from './components/ProductGallery.vue'
+import PageLink from './components/PageLink.vue'
+import AppInfo from './components/AppInfo.vue'
 export default {
   name: 'App',
   components: {
     AppHeader,
     ProductGallery,
+    PageLink,
+    AppInfo
   }
 }
 </script>
@@ -13,6 +17,8 @@ export default {
 <template>
   <AppHeader/>
   <ProductGallery/>
+  <PageLink/>
+  <AppInfo/>
 </template>
 
 <style lang="scss">
