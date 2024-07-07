3b707dd85cf0170cba9fd61cc248e9119188fd64
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-08 00:12:46 +0200

    feat: add banner blu link

--- src/components/PageLink.vue
+++ src/components/PageLink.vue
@@ -7,26 +7,31 @@ export default {
                 {
                     image: 'src/assets/buy-comics-digital-comics.png',
                     url: '#',
+                    text: 'DIGITAL COMICS',
                     current: false,
                 },
                 {
                     image: 'src/assets/buy-comics-merchandise.png',
                     url: '#',
+                    text: 'DC MERCHANDISE',
                     current: false,
                 },
                 {
                     image: 'src/assets/buy-comics-subscriptions.png',
                     url: '#',
+                    text: 'SUBSCRIPTION',
                     current: false,
                 },
                 {
                     image: 'src/assets/buy-comics-shop-locator.png',
                     url: '#',
+                    text: 'COMICS SHOP LOCATOR',
                     current: false,
                 },
                 {
                     image: 'src/assets/buy-dc-power-visa.svg',
                     url: '#',
+                    text: 'DC POWER VISA',
                     current: false,
                 },
             ]
@@ -40,7 +45,12 @@ export default {
         <div class="container">
             <ul>
                 <li v-for="(image, index) in imgLink" key="index">
-                    <img :src="image.image" alt="">
+                    <div class="icone">
+                        <img :src="image.image" alt="">
+                    </div>
+                    <div>
+                        <a href="#"> {{ image.text }}</a>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -54,6 +64,32 @@ export default {
 
 .container {
     background-color: $primary;
+    padding: 2rem 17rem;
+
+    ul {
+        @include center('horizontal');
+        justify-content: space-between ;
+        
+        li {
+            list-style: none;
+            @include center();
+            gap: 1rem;
+            
+            .icone {
+                width: 40px;
+            }
+            
+            img {
+                width: 100%;
+            }
+
+            a {
+                text-decoration: none;
+                font-size: 0.7rem;
+                color: white;
+            }
+        }
+    }
 }
 
 </style>
